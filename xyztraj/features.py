f0e170d593c9d0cfabaf573b7cef8d35ba96cf13
jrossyra <jrossyra@gmail.com>
2019-02-28 01:54:36 -0500

    [features] angle and distance added

--- xyztraj/features.py
+++ xyztraj/features.py
@@ -1,7 +1,36 @@
 
 import numpy as np
 
+# Nice name for each feature, users should import from
+# here to make features in their own scripts
 from xyztraj.featurize import _feature as feature
+
+
+def angle(trajectory_array, atom_indices=None):
+    return feature(_angle, trajectory_array, atom_indices)()
+
+
+def _angle(points):
+    '''Order is important
+    '''
+    if len(points.shape) == 2:
+        assert points.shape[1] == 3
+        p0, p1, p2 = points
+
+    elif points.shape[0] == 9:
+        assert len(points.shape) == 1
+        p0, p1, p2 = [points[i*3:(i+1)*3] for i in range(3)]
+
+    else:
+        raise ValueError("Angle calculation requires a (possibly flattened) 3x3 array")
+
+    return np.degrees(np.arccos(
+        np.dot(p1-p0, p2-p1)/np.linalg.norm(p0-p1)/np.linalg.norm(p2-p1)
+    ))
+    
+
+def nofeature(trajectory_array, atom_indices=None):
+    return feature(lambda x: x, trajectory_array, atom_indices)()
 
 
 def dihedral(trajectory_array, atom_indices=None):
@@ -11,7 +40,26 @@ def dihedral(trajectory_array, atom_indices=None):
     feature_trajectory :: numpy array (mxd)
     Trajectory array of m frames each with d-dimensional feature value
     ''' 
+    # TODO do type check/asserts in here?
+    #       - yes, fail early
+    #       - no, more flexible?
     return feature(_dihedral, trajectory_array, atom_indices)()
+
+
+def distance(trajectory_array, atom_indices=None):
+    '''Array of 3D norms of timeseries of 2 points 
+    '''
+    # Not doing this way, faster implementation is easy
+    #return feature(_distance, trajectory_array, atom_indices)()
+    assert trajectory_array.shape[2] == 3
+
+    if atom_indices is None:
+        assert trajectory_array.shape[1] == 2
+        atom_indices = [0,1]
+
+    a1, a2 = [trajectory_array[:,ai,:] for ai in atom_indices]
+
+    return np.linalg.norm(a1-a2, axis=1)
 
 
 def _dihedral(points):
@@ -34,7 +82,7 @@ def _dihedral(points):
         p0, p1, p2, p3 = [points[i*3:(i+1)*3] for i in range(4)]
 
     else:
-        raise ValueError("Dihedral calculation requires 4x3 array")
+        raise ValueError("Dihedral calculation requires a (possibly flattened)  4x3 array")
 
     b0 = -1.0*(p1 - p0)
     b1 = p2 - p1
@@ -58,3 +106,4 @@ def _dihedral(points):
     y = np.dot(np.cross(b1, v), w)
 
     return np.degrees(np.arctan2(y, x))
+
