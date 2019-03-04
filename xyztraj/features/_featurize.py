07dc898c49a3ca501367964ec272ba2352d92f9c
jrossyra <jrossyra@gmail.com>
2019-03-03 23:03:32 -0500

    [features] input format enforcement decorator for feature calculators

--- xyztraj/features/_featurize.py
+++ xyztraj/features/_featurize.py
@@ -25,30 +25,32 @@ def featurizer(corefunc):
         nframes, natoms, ncoords = trajectory_array.shape
         assert ncoords == 3
 
-        return trajectory_array.reshape(nframes, natoms*ncoords)
-
+        return trajectory_array.reshape(nframes, natoms * ncoords)
 
     def _featurizer(featurefunc):
         def wrapper(array, *args, **kwargs):
 
             assert isinstance(array, np.ndarray)
             featurized = np.apply_along_axis(
-              corefunc, 1, _flatten_atomcoords(featurefunc(array, *args, **kwargs))
+                corefunc, 1, _flatten_atomcoords(
+                    featurefunc(array, *args, **kwargs))
             )
             return featurized
+
         return wrapper
 
     return _featurizer
 
 
-def _feature(calculation_on_atomcoords, trajectory_array, atom_indices=None):
+def _feature(calculation_on_atomcoords, trajectory_array,
+             atom_indices=None):
 
     @featurizer(calculation_on_atomcoords)
     def _trajectory(trajectory_array, atom_indices=None):
         if atom_indices:
-            return trajectory_array[:,atom_indices,:]
+            return trajectory_array[:, atom_indices, :]
+
         else:
             return trajectory_array
-    
-    return lambda: _trajectory(trajectory_array, atom_indices)
 
+    return lambda: _trajectory(trajectory_array, atom_indices)
