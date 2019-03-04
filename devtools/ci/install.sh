277b59c131df159da5bd0674b2d7f838df5f1623
jrossyra <jrossyra@gmail.com>
2019-03-04 15:06:56 -0500

    added missing build env

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -3,13 +3,16 @@
 # taken from https://raw.githubusercontent.com/broadinstitute/viral-ngs/master/travis/install-conda.sh
 set -e
 
-echo "Starting Miniconda Download"
 # the miniconda directory may exist if it has been restored from cache
 if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
+
     echo "Miniconda install already present from cache: $MINICONDA_DIR"
     export PATH="$MINICONDA_DIR/bin:$PATH"
     hash -r
+
 else # if it does not exist, we need to install miniconda
+
+    echo "Starting Miniconda Download"
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
 
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
