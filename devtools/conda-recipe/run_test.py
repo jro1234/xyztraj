eb308248770f6ed97e5cc681175b3ee7413ef1f4
jrossyra <jrossyra@gmail.com>
2019-03-04 22:28:41 -0500

    fix syntaxerror

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -1,3 +1,4 @@
+# noqa E902
 import os
 import sys
 
@@ -7,14 +8,16 @@ import pytest
 test_pkg = 'xyztraj'
 cover_pkg = test_pkg
 
+build_dir = os.getenv('TRAVIS_BUILD_DIR', os.path.expanduser('~'))
+
 # where to write reports
-reports_dir = os.path.join(
-    os.getenv('TRAVIS_BUILD_DIR', os.path.expanduser('~')), 'reports')
+reports_dir = os.path.join(build_dir, 'reports')
 
 # reports
 junit_xml = os.path.join(reports_dir, 'junit.xml')
 coverage_xml = os.path.join(reports_dir, 'coverage.xml')
 
+print("Travis build directory: ", os.getcwd())
 print("Current directory: ", os.getcwd())
 print("Reports directory: ", reports_dir)
 print("Reports already exists? ", os.path.exists(reports_dir))
@@ -24,7 +27,8 @@ if not os.path.exists(reports_dir):
     print('junit destination:', junit_xml)
     njobs_args = '-p no:xdist' if os.getenv('TRAVIS') else '-n2'
 
-    pytest_args = ("-v --pyargs {test_pkg} "
+    pytest_args = (
+        "-v --pyargs {test_pkg} "
         "--cov={cover_pkg} "
         "--cov-report=xml:{dest_report} "
         "--doctest-modules "
@@ -34,13 +38,11 @@ if not os.path.exists(reports_dir):
         #"--durations=20 "
         .format(test_pkg=test_pkg, cover_pkg=cover_pkg,
                 junit_xml=junit_xml, pytest_cfg='setup.cfg',
-                dest_report=coverage_xml),
                 njobs_args=njobs_args,
-        )
-        .split(' '))
+                dest_report=coverage_xml),
+    ).split(' ')
 
     print("args:", pytest_args)
     res = pytest.main(pytest_args)
 
     sys.exit(res)
-
