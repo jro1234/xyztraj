a59461597e136bb37b9e688c0b4bdcd6b7e93fbe
Travis CI User <travis@example.org>
2019-03-07 19:14:23 +0000

    [tests] remove read setup.cfg to fix configuration

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -18,34 +18,22 @@ reports_dir = os.path.join(build_dir, 'reports')
 junit_xml = os.path.join(reports_dir, 'junit.xml')
 coverage_xml = os.path.join(reports_dir, 'coverage.xml')
 
-print("Current directory: ", cwd)
-print("Travis build directory: ", build_dir)
-print("Reports directory: ", reports_dir)
-print("Reports already exists? ", os.path.exists(reports_dir))
-
 if not os.path.exists(reports_dir):
     os.makedirs(reports_dir)
     print('junit destination:', junit_xml)
-    njobs_args = '-p no:xdist' if os.getenv('TRAVIS') else '-n2'
 
     pytest_args = (
         "-v --pyargs {test_pkg} "
         "--cov={cover_pkg} "
         "--cov-report=xml:{dest_report} "
         "--doctest-modules "
-        "{njobs_args} "
         "--junit-xml={junit_xml} "
-        "-c {pytest_cfg}"
         #"--durations=20 "
         .format(test_pkg=test_pkg, cover_pkg=cover_pkg,
-                junit_xml=junit_xml, pytest_cfg='setup.cfg',
-                njobs_args=njobs_args,
-                dest_report=coverage_xml
+                dest_report=coverage_xml, junit_xml=junit_xml,
         ).split(' '))
 
     print("args:", pytest_args)
-    #os.chdir(
     res = pytest.main(pytest_args)
-    #os.chdir(cwd)
 
     sys.exit(res)
