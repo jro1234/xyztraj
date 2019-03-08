06b827133791ea130dfc15cd47fef7ecd7099ed7
Travis CI User <travis@example.org>
2019-03-08 03:49:56 +0000

    [tests] dihedral feature

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -11,7 +11,13 @@ class TestClass(object):
     # FIXME this line should work
     #self.traj = reader.readfile('tests/data/rnatraj.xyz')
     traj = reader.readfile('tests/data/rnatraj.xyz', nframes=1000)
-    traj_len_answer = 100
+    trajdims_answer = [100, 632, 3]
 
-    def test_trajlength(self):
+    def test_trajdims(self):
         assert self.traj.trajectory.shape[0] == self.traj_len_answer
+
+    def test_dihedral(self):
+        chi_atoms = (0, 3, 13, 18)
+        traj_chi = xyztraj.features.dihedral(
+            self.traj.trajectory, chi_atoms)
+        assert traj_chi.shape == (self.trajdims_answer[0], 1)
